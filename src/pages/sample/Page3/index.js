b025fe0f826d9ebb27743dbd0bdf98aa7247f1c6
Zunayed <zunayedology@gmail.com>
2024-05-28 12:05:46 +0600

    Search on column added in Page3

--- src/pages/sample/Page3/index.js
+++ src/pages/sample/Page3/index.js
@@ -1,72 +1,182 @@
-import React from 'react';
-import {
-  CartesianGrid,
-  Legend,
-  Line,
-  LineChart,
-  ResponsiveContainer,
-  Tooltip,
-  AreaChart,
-  Area,
-  XAxis,
-  YAxis,
-} from 'recharts';
+import React, {useRef, useState} from 'react';
+import {SearchOutlined} from '@ant-design/icons';
+import {Button, Input, Space, Table} from 'antd';
+import Highlighter from 'react-highlight-words';
+import PropTypes from 'prop-types';
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+  },
+  {
+    key: '2',
+    name: 'Joe Black',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+  },
+  {
+    key: '3',
+    name: 'Jim Green',
+    age: 32,
+    address: 'Sydney No. 1 Lake Park',
+  },
+  {
+    key: '4',
+    name: 'Jim Red',
+    age: 32,
+    address: 'London No. 2 Lake Park',
+  },
+];
+const App = () => {
+  const [searchText, setSearchText] = useState('');
+  const [searchedColumn, setSearchedColumn] = useState('');
+  const searchInput = useRef(null);
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+  };
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    setSearchText('');
+  };
+  const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({
+      // eslint-disable-next-line react/prop-types
+      setSelectedKeys,
+      // eslint-disable-next-line react/prop-types
+      selectedKeys,
+      // eslint-disable-next-line react/prop-types
+      confirm,
+      // eslint-disable-next-line react/prop-types
+      clearFilters,
+      // eslint-disable-next-line react/prop-types
+      close,
+    }) => (
+      <div
+        style={{
+          padding: 8,
+        }}
+        onKeyDown={(e) => e.stopPropagation()}>
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{
+            marginBottom: 8,
+            display: 'block',
+          }}
+        />
+        <Space>
+          <Button
+            type='primary'
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size='small'
+            style={{
+              width: 90,
+            }}>
+            Search
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size='small'
+            style={{
+              width: 90,
+            }}>
+            Reset
+          </Button>
+          <Button
+            type='link'
+            size='small'
+            onClick={() => {
+              confirm({
+                closeDropdown: false,
+              });
+              setSearchText(selectedKeys[0]);
+              setSearchedColumn(dataIndex);
+            }}>
+            Filter
+          </Button>
+          <Button
+            type='link'
+            size='small'
+            onClick={() => {
+              close();
+            }}>
+            close
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined
+        style={{
+          color: filtered ? '#1677ff' : undefined,
+        }}
+      />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+    onFilterDropdownOpenChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.current?.select(), 100);
+      }
+    },
+    render: (text) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{
+            backgroundColor: '#ffc069',
+            padding: 0,
+          }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ''}
+        />
+      ) : (
+        text
+      ),
+  });
 
-const SimpleLineChart = () => {
-  const data1 = [
-    {name: 'A', pv: getRandomValue(), uv: getRandomValue()},
-    {name: 'B', pv: getRandomValue(), uv: getRandomValue()},
-    {name: 'C', pv: getRandomValue(), uv: getRandomValue()},
-    {name: 'D', pv: getRandomValue(), uv: getRandomValue()},
-    {name: 'E', pv: getRandomValue(), uv: getRandomValue()},
+  getColumnSearchProps().propTypes = {
+    setSelectedKeys: PropTypes.func.isRequired,
+    selectedKeys: PropTypes.array.isRequired,
+    confirm: PropTypes.func.isRequired,
+    clearFilters: PropTypes.func.isRequired,
+    close: PropTypes.func.isRequired,
+  };
+
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      width: '30%',
+      ...getColumnSearchProps('name'),
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+      key: 'age',
+      width: '20%',
+      ...getColumnSearchProps('age'),
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+      ...getColumnSearchProps('address'),
+      sorter: (a, b) => a.address.length - b.address.length,
+      sortDirections: ['descend', 'ascend'],
+    },
   ];
-  const data2 = [
-    {name: 'F', pv: getRandomValue(), uv: getRandomValue()},
-    {name: 'G', pv: getRandomValue(), uv: getRandomValue()},
-    {name: 'H', pv: getRandomValue(), uv: getRandomValue()},
-    {name: 'I', pv: getRandomValue(), uv: getRandomValue()},
-    {name: 'J', pv: getRandomValue(), uv: getRandomValue()},
-  ];
-  function getRandomValue() {
-    return Math.floor(Math.random() * 100);
-  }
-
-  return (
-    <>
-      <ResponsiveContainer width='100%' height={200}>
-        <LineChart
-          data={data1}
-          margin={{top: 10, right: 0, left: -25, bottom: 0}}>
-          <XAxis dataKey='name' />
-          <YAxis />
-          <CartesianGrid strokeDasharray='3 3' />
-          <Tooltip />
-          <Legend />
-          <Line
-            type='monotone'
-            dataKey='pv'
-            stroke='#4299E1'
-            activeDot={{r: 8}}
-          />
-          <Line type='monotone' dataKey='uv' stroke='#F04F47' />
-        </LineChart>
-      </ResponsiveContainer>
-
-      <ResponsiveContainer width='100%' height={200}>
-        <AreaChart
-          data={data2}
-          margin={{top: 10, right: 0, left: -25, bottom: 0}}>
-          <XAxis dataKey='name' />
-          <YAxis />
-          <CartesianGrid strokeDasharray='3 3' />
-          <Tooltip />
-          <Legend />
-          <Area type='monotone' dataKey='pv' fill='#4299E1' stroke='#4299E1' />
-          <Area type='monotone' dataKey='uv' fill='#F04F47' stroke='#F04F47' />
-        </AreaChart>
-      </ResponsiveContainer>
-    </>
-  );
+  return <Table columns={columns} dataSource={data} />;
 };
-
-export default SimpleLineChart;
+export default App;
