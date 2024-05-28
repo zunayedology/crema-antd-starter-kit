e6d974b645fd1bd34f36002ca5e893ed886f746f
Zunayed <zunayedology@gmail.com>
2024-05-28 15:38:02 +0600

    Page3 updated

--- src/pages/sample/Page3/index.js
+++ src/pages/sample/Page3/index.js
@@ -1,8 +1,155 @@
+// import React, {useRef, useState} from 'react';
+// import {SearchOutlined} from '@ant-design/icons';
+// import {Button, Input, Space, Table} from 'antd';
+// import Highlighter from 'react-highlight-words';
+//
+// const data = [
+//   {
+//     key: '1',
+//     name: 'John Brown',
+//     age: 32,
+//     address: 'New York No. 1 Lake Park',
+//   },
+//   {
+//     key: '2',
+//     name: 'Joe Black',
+//     age: 42,
+//     address: 'London No. 1 Lake Park',
+//   },
+//   {
+//     key: '3',
+//     name: 'Jim Green',
+//     age: 32,
+//     address: 'Sydney No. 1 Lake Park',
+//   },
+//   {
+//     key: '4',
+//     name: 'Jim Red',
+//     age: 32,
+//     address: 'London No. 2 Lake Park',
+//   },
+// ];
+//
+// const App = () => {
+//   const [searchText, setSearchText] = useState('');
+//   const [searchedColumn, setSearchedColumn] = useState('');
+//   const [dataList, setDataList] = useState(data);
+//   const searchInput = useRef(null);
+//
+//   const handleSearch = (selectedKeys, confirm, dataIndex, value) => {
+//     confirm();
+//     setSearchText(selectedKeys[0]);
+//     setSearchedColumn(dataIndex);
+//     console.log('Check handle search', selectedKeys);
+//     setDataList(data?.filter((x) => x.name === value));
+//   };
+//
+//   const handleReset = (clearFilters) => {
+//     clearFilters();
+//     setSearchText('');
+//     setSearchedColumn('');
+//   };
+//
+//   const getColumnSearchProps = (dataIndex) => ({
+//     filterDropdown: ({
+//       // eslint-disable-next-line react/prop-types
+//       setSelectedKeys,
+//       // eslint-disable-next-line react/prop-types
+//       selectedKeys,
+//       // eslint-disable-next-line react/prop-types
+//       confirm,
+//       // eslint-disable-next-line react/prop-types
+//       clearFilters,
+//     }) => (
+//       <div style={{padding: 8}} onKeyDown={(e) => e.stopPropagation()}>
+//         <Input
+//           ref={searchInput}
+//           placeholder={`Search ${dataIndex}`}
+//           value={selectedKeys[0]}
+//           onChange={(e) =>
+//             setSelectedKeys(e.target.value ? [e.target.value] : [])
+//           }
+//           onPressEnter={() =>
+//             handleSearch(selectedKeys, confirm, dataIndex, selectedKeys[0])
+//           }
+//           style={{marginBottom: 8, display: 'block'}}
+//         />
+//         <Space>
+//           <Button
+//             type='primary'
+//             onClick={() =>
+//               handleSearch(selectedKeys, confirm, dataIndex, selectedKeys[0])
+//             }
+//             icon={<SearchOutlined />}
+//             size='small'
+//             style={{width: 90}}>
+//             Search
+//           </Button>
+//           <Button
+//             onClick={() => clearFilters && handleReset(clearFilters)}
+//             size='small'
+//             style={{width: 90}}>
+//             Reset
+//           </Button>
+//         </Space>
+//       </div>
+//     ),
+//     filterIcon: (filtered) => (
+//       <SearchOutlined style={{color: filtered ? '#1677ff' : undefined}} />
+//     ),
+//     onFilterDropdownOpenChange: (visible) => {
+//       if (visible) {
+//         setTimeout(() => searchInput.current?.select(), 100);
+//       }
+//     },
+//     render: (text) =>
+//       searchedColumn === dataIndex ? (
+//         <Highlighter
+//           highlightStyle={{backgroundColor: '#ffc069', padding: 0}}
+//           searchWords={[searchText]}
+//           autoEscape
+//           textToHighlight={text ? text.toString() : ''}
+//         />
+//       ) : (
+//         text
+//       ),
+//   });
+//
+//   const columns = [
+//     {
+//       title: 'Name',
+//       dataIndex: 'name',
+//       key: 'name',
+//       width: '30%',
+//       ...getColumnSearchProps('name'),
+//     },
+//     {
+//       title: 'Age',
+//       dataIndex: 'age',
+//       key: 'age',
+//       width: '20%',
+//       ...getColumnSearchProps('age'),
+//     },
+//     {
+//       title: 'Address',
+//       dataIndex: 'address',
+//       key: 'address',
+//       ...getColumnSearchProps('address'),
+//       sorter: (a, b) => a.address.length - b.address.length,
+//       sortDirections: ['descend', 'ascend'],
+//     },
+//   ];
+//
+//   return <Table columns={columns} dataSource={dataList} />;
+// };
+//
+// export default App;
+
 import React, {useRef, useState} from 'react';
 import {SearchOutlined} from '@ant-design/icons';
 import {Button, Input, Space, Table} from 'antd';
 import Highlighter from 'react-highlight-words';
-import PropTypes from 'prop-types';
+
 const data = [
   {
     key: '1',
@@ -29,19 +176,28 @@ const data = [
     address: 'London No. 2 Lake Park',
   },
 ];
+
 const App = () => {
   const [searchText, setSearchText] = useState('');
   const [searchedColumn, setSearchedColumn] = useState('');
   const searchInput = useRef(null);
+  const [, setFilters] = useState({});
+
   const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    const newFilters = {[dataIndex]: selectedKeys[0]};
     confirm();
     setSearchText(selectedKeys[0]);
     setSearchedColumn(dataIndex);
+    setFilters(newFilters);
   };
+
   const handleReset = (clearFilters) => {
     clearFilters();
     setSearchText('');
+    setSearchedColumn('');
+    setFilters({});
   };
+
   const getColumnSearchProps = (dataIndex) => ({
     filterDropdown: ({
       // eslint-disable-next-line react/prop-types
@@ -52,14 +208,8 @@ const App = () => {
       confirm,
       // eslint-disable-next-line react/prop-types
       clearFilters,
-      // eslint-disable-next-line react/prop-types
-      close,
     }) => (
-      <div
-        style={{
-          padding: 8,
-        }}
-        onKeyDown={(e) => e.stopPropagation()}>
+      <div style={{padding: 8}} onKeyDown={(e) => e.stopPropagation()}>
         <Input
           ref={searchInput}
           placeholder={`Search ${dataIndex}`}
@@ -68,10 +218,7 @@ const App = () => {
             setSelectedKeys(e.target.value ? [e.target.value] : [])
           }
           onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
-          style={{
-            marginBottom: 8,
-            display: 'block',
-          }}
+          style={{marginBottom: 8, display: 'block'}}
         />
         <Space>
           <Button
@@ -79,48 +226,20 @@ const App = () => {
             onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
             icon={<SearchOutlined />}
             size='small'
-            style={{
-              width: 90,
-            }}>
+            style={{width: 90}}>
             Search
           </Button>
           <Button
             onClick={() => clearFilters && handleReset(clearFilters)}
             size='small'
-            style={{
-              width: 90,
-            }}>
+            style={{width: 90}}>
             Reset
-          </Button>
-          <Button
-            type='link'
-            size='small'
-            onClick={() => {
-              confirm({
-                closeDropdown: false,
-              });
-              setSearchText(selectedKeys[0]);
-              setSearchedColumn(dataIndex);
-            }}>
-            Filter
-          </Button>
-          <Button
-            type='link'
-            size='small'
-            onClick={() => {
-              close();
-            }}>
-            close
           </Button>
         </Space>
       </div>
     ),
     filterIcon: (filtered) => (
-      <SearchOutlined
-        style={{
-          color: filtered ? '#1677ff' : undefined,
-        }}
-      />
+      <SearchOutlined style={{color: filtered ? '#1677ff' : undefined}} />
     ),
     onFilter: (value, record) =>
       record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
@@ -132,10 +251,7 @@ const App = () => {
     render: (text) =>
       searchedColumn === dataIndex ? (
         <Highlighter
-          highlightStyle={{
-            backgroundColor: '#ffc069',
-            padding: 0,
-          }}
+          highlightStyle={{backgroundColor: '#ffc069', padding: 0}}
           searchWords={[searchText]}
           autoEscape
           textToHighlight={text ? text.toString() : ''}
@@ -145,14 +261,6 @@ const App = () => {
       ),
   });
 
-  getColumnSearchProps().propTypes = {
-    setSelectedKeys: PropTypes.func.isRequired,
-    selectedKeys: PropTypes.array.isRequired,
-    confirm: PropTypes.func.isRequired,
-    clearFilters: PropTypes.func.isRequired,
-    close: PropTypes.func.isRequired,
-  };
-
   const columns = [
     {
       title: 'Name',
@@ -160,6 +268,7 @@ const App = () => {
       key: 'name',
       width: '30%',
       ...getColumnSearchProps('name'),
+      filteredValue: searchedColumn === 'name' ? [searchText] : null,
     },
     {
       title: 'Age',
@@ -167,6 +276,7 @@ const App = () => {
       key: 'age',
       width: '20%',
       ...getColumnSearchProps('age'),
+      filteredValue: searchedColumn === 'age' ? [searchText] : null,
     },
     {
       title: 'Address',
@@ -175,8 +285,11 @@ const App = () => {
       ...getColumnSearchProps('address'),
       sorter: (a, b) => a.address.length - b.address.length,
       sortDirections: ['descend', 'ascend'],
+      filteredValue: searchedColumn === 'address' ? [searchText] : null,
     },
   ];
+
   return <Table columns={columns} dataSource={data} />;
 };
+
 export default App;
